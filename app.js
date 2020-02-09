b75185173baabfa1da6f81dc1e3751b018163959
root <amirf2@gmail.com>
2020-02-09 02:48:30 +0000

    update

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use((req, res, next) => {
 /*---------------------------------------------------------------------
                        Routes  Configurations
  ---------------------------------------------------------------------*/
-
+ 
 
 app.use("/", indexRoutes);
 app.use("/", rollerCoasterListsRoutes);
